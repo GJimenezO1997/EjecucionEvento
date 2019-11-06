9eee9998ff154c885049981c65b2d632343b8c25
Gustavo Jiménez Oaxaca <39020502+GJimenezO1997@users.noreply.github.com>
2019-11-06 17:44:29 -0600

    Add files via upload

---
+++ time_pictureBox/time_pictureBox/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace time_pictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_horario_programada = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hora_actual = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.programar_horario = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa el horario a ejecutar:";
+            // 
+            // txt_horario_programada
+            // 
+            this.txt_horario_programada.Location = new System.Drawing.Point(6, 61);
+            this.txt_horario_programada.Name = "txt_horario_programada";
+            this.txt_horario_programada.Size = new System.Drawing.Size(140, 20);
+            this.txt_horario_programada.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 402);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(665, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hora:";
+            // 
+            // hora_actual
+            // 
+            this.hora_actual.AutoSize = true;
+            this.hora_actual.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hora_actual.Location = new System.Drawing.Point(724, 40);
+            this.hora_actual.Name = "hora_actual";
+            this.hora_actual.Size = new System.Drawing.Size(46, 17);
+            this.hora_actual.TabIndex = 5;
+            this.hora_actual.Text = "label3";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // programar_horario
+            // 
+            this.programar_horario.Location = new System.Drawing.Point(169, 59);
+            this.programar_horario.Name = "programar_horario";
+            this.programar_horario.Size = new System.Drawing.Size(140, 23);
+            this.programar_horario.TabIndex = 6;
+            this.programar_horario.Text = "Programar horario";
+            this.programar_horario.UseVisualStyleBackColor = true;
+            this.programar_horario.Click += new System.EventHandler(this.programar_horario_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_horario_programada);
+            this.groupBox1.Controls.Add(this.hora_actual);
+            this.groupBox1.Controls.Add(this.programar_horario);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(798, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuración";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "HH:mm";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 559);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Horario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_horario_programada;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label hora_actual;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button programar_horario;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ time_pictureBox/time_pictureBox/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace time_pictureBox
+{
+    public partial class Form1 : Form
+    {
+        
+        int hora, minuto;
+        int hora_ingresada = 0, minuto_ingresado = 0;
+        int hora_ingresada1 = 0, minuto_ingresado1 = 0;
+        int hora_evaluacion = 0, minuto_evaluacion = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Inicializar el timer para que la hora empiece a correr 
+            timer1.Start();
+
+            TimeSpan horas = TimeSpan.Parse(DateTime.Now.ToString("HH:mm:ss"));
+            
+            //Seprar el tiempo en horas y minutos
+            String str_horas = horas.ToString().Remove(2);
+            String str_minutos = horas.ToString().Remove(0, 3);
+            String str_min_sin_segundos = str_minutos.ToString().Remove(2);
+
+            //Convertir horas y minutos a enteros para evaluarlos
+            hora = int.Parse(str_horas);
+            minuto = int.Parse(str_min_sin_segundos);
+            
+            //Establecer en el extBox el horario actual
+            txt_horario_programada.Text = str_horas + ":" + str_min_sin_segundos;
+            
+            //Obetener imagen del textbox
+            Image imagen = Image.FromFile("img.png");
+            pictureBox1.Image = imagen;
+            pictureBox1.Visible = false;
+
+        }
+        
+        //Declarar timer para obtener la hora y poderla ejecutar en tiempo real
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            TimeSpan tiempo_actual = TimeSpan.Parse(DateTime.Now.ToString("HH:mm:ss"));
+            this.hora_actual.Text = tiempo_actual.ToString();
+
+            TimeSpan timepo_de_activacion = new TimeSpan(hora_ingresada, minuto_ingresado, 0);
+            Console.WriteLine(timepo_de_activacion.ToString());
+
+            if (tiempo_actual == timepo_de_activacion)
+            {
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void programar_horario_Click(object sender, EventArgs e)
+        {
+            //Obtener datos el textbox para evaluarlos con la hora actual
+            String str_horario_programado = txt_horario_programada.Text;
+            String str_hora_programada = str_horario_programado.Remove(2);
+            String str_minuto_programado = str_horario_programado.Remove(0, 3);
+            
+            hora_ingresada1 = int.Parse(str_hora_programada);
+            minuto_ingresado1 = int.Parse(str_minuto_programado);
+
+            if(hora_ingresada1 > 23 || hora_ingresada1 < 0 || minuto_ingresado1 > 59 || minuto_ingresado1 < 0)
+            {
+                MessageBox.Show("Ingresa la hora o minutos correctos.", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                hora_ingresada = hora_ingresada1;
+                minuto_ingresado = minuto_ingresado1;
+                MessageBox.Show("Hora programada, en un momento aparecera su imagen.");
+            }
+        }
+    }
+}
